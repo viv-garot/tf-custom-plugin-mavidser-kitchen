61205e5ec303a8a35c287af31def1f169005640d
viv-garot <vivien@hashicorp.com>
2021-08-04 18:37:03 +0200

    Added missing test folder

---
+++ test/integration/default/check-plugin.rb
@@ -0,0 +1,14 @@
+describe file('/home/vagrant/.terraform.d/plugins/localproviders/mavidser/linux/1.0.2/linux_amd64/terraform-provider-linux') do
+  it {should exist}
+  its('md5sum') { should eq 'f46802e787ae030a81a5db01d9ae254f' }
+  its('size') { should eq 24120848 }
+  its('type') { should eq :file }
+  its('owner') { should eq 'vagrant' }
+  its('group') { should eq 'vagrant' }
+end
+
+describe command('terraform init --upgrade=true && terraform apply --auto-approve=true') do
+  its('stdout') {should match /testgroup-id = "1001"/ }
+  its('stdout') {should match /testuser1-id = "1001"/ }
+  its('stdout') {should match /testuser2-id = "1002"/ }
+end
